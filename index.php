9294032fca10cf03fb23150b1b98d67295989f3e
Pieterjan van Dijk <Pieterjan.v.dijk@gmail.com>
2020-11-24 14:53:33 +0100

    initial commit, front end not completed yet, connection database succesfull

--- index.php
+++ index.php
@@ -5,53 +5,74 @@
 	<link rel="stylesheet" type="text/css" href="styles/index.css">
 </head>
 <body>
-	<div id='card'>
-		<h1 id='title'>title</h1>
-		<button id='options'>options</button>
-		<div id='content'>
-			<div class='content'>
-				<p id='text'>content here</p>
-				<button id='expandbutton'>expand</button>
+	<?php
+		include "database/connect.php";
+	?>
+
+
+
+
+
+	<div>
+		<a href="www.google.com"><h2>naam van het bord</h2></a>
+
+		<div id='row1' class='primary_rows'>
+			<h3>title goes brrrrrrrrrr</h3>
+			<div class='cards'>
+				<div>
+					<p>create buttons</p>
+				</div>
+				<div>
+					<p>try the buttons</p>
+				</div>
+				<div>
+					<p>shit on buttons</p>
+				</div>
+				<div>
+					<p>buttons is my favourite cat in the whole world, his favourite food is bacon and my dads shoes.</p>
+				</div>
 			</div>
-			<div class='content'>
-				<p id='text'>more content here</p>
-				<button id='expandbutton'>expand</button>
-			</div>
-			<div class='content'>
-				<p id='text'>even more content here</p>
-				<button id='expandbutton'>expand</button>
+			<div class='add_card_button'>
+				<p> + kaart toevoegen</p>
 			</div>
 		</div>
-	</div>
-	<div id='content'>
-		<?php
 
-
-			$servername = "localhost";
-			$username = "root";
-			$password = "mysql";
-			$dbname = 'to_do';
-
-			try{
-			    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-			    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			    
-
-				echo '<script>console.log("';
-			    echo "Connected successfully";
-				echo '");</script>';
-		    }
-			catch(PDOException $e){
-		    	echo "Connection failed: " . $e->getMessage();
-		    }
-		    $sql = "SELECT card_id, title, background_color, content FROM cards";
-		    $stmt = $conn->prepare($sql);
-		    $stmt->execute();
-
-		    while ($data = $stmt->fetch())
-		    	echo '<p>' . $data["card_id"] . $data["title"] . $data["background_color"] . $data["content"] . '</p>';
-		?>
-	</div>
-	<a href="new-card.php"><img border="0" alt="add" src="img/pictograms/plus-white.png" width="100" height="100"></a>
+		<div id='row2' class='primary_rows'>
+			<h3>title goes brrrrrrrrrr</h3>
+			<div>
+				<p>create buttons</p>
+			</div>
+			<div>
+				<p>try the buttons</p>
+			</div>
+			<div>
+				<p>shit on buttons</p>
+			</div>
+			<div>
+				<p>buttons is my favourite cat in the whole world, his favourite food is bacon and my dads shoes.</p>
+			</div>
+			<div class='add_card_button'>
+				<p> + kaart toevoegen</p>
+			</div>
+		</div>
+		
+		<div id='row3' class='primary_rows'>
+			<h3>title goes brrrrrrrrrr</h3>
+			<div>
+				<p>create buttons</p>
+			</div>
+			<div>
+				<p>try the buttons</p>
+			</div>
+			<div>
+				<p>shit on buttons</p>
+			</div>
+			<div>
+				<p>buttons is my favourite cat in the whole world, his favourite food is bacon and my dads shoes.</p>
+			</div>
+			<div class='add_card_button'>
+				<p> + kaart toevoegen</p>
+			</div>
+		</div>
 </body>
 </html>
